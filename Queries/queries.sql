8f7f79ae8ee99a416886dcab04ebec71e5afe972
Elizabeth Szathmary <elizabeth.szathmary@gmail.com>
2020-03-01 07:55:13 -0800

    updating work

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,4 +1,43 @@
 
+
+--Return a list of employees eligable for retirement
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+--Return a list of employees born in 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Put data into retirement table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
 DROP TABLE retirement_info;
 
 -- Create new table for retiring employees
@@ -7,11 +46,51 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
 -- Check the table
 SELECT * FROM retirement_info;
 
--- Inner Join: Joining departments and dept_manager tables
+-- Inner Join:  bsdaJoining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+--Left join
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+SELECT * FROM retirement_info;
+
+--name aliases
+--The to_date column returned NUL when I ran this.
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name,
+	de.to_date 
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- Resetting the departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+--Renaming tables with new alias
+--to_date column looks good this time
+--The spacing is also better with this line of code.
 SELECT d.dept_name,
      dm.emp_no,
      dm.from_date,
@@ -20,16 +99,8 @@ FROM departments as d
 INNER JOIN dept_manager as dm
 ON d.dept_no = dm.dept_no;
 
--- Left Join: Joining retirement_info and dept_emp tables
-SELECT retirement_info.emp_no,
-	retirement_info.first_name,
-retirement_info.last_name,
-	dept_emp.to_date
-FROM retirement_info
-LEFT JOIN dept_emp
-ON retirement_info.emp_no = dept_emp.emp_no;
-
--- New table containing only current employees
+--Left join of retirment_info and dept_emp tables using aliases to 
+--ensure that all employees in the list are still employed
 SELECT ri.emp_no,
 	ri.first_name,
 	ri.last_name,
@@ -40,7 +111,7 @@ LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 
-SELECT * FROM current_emp;
+--Using Count, Group By and Order by:
 
 -- Employee count by department number
 SELECT COUNT(ce.emp_no), de.dept_no
@@ -48,3 +119,5 @@ FROM current_emp as ce
 LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no;
+
+
