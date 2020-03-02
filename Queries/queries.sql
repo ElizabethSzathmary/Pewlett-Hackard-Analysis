ce4d3c1526018b0900db9bd75805609c428b5fcb
Elizabeth Szathmary <elizabeth.szathmary@gmail.com>
2020-03-01 17:10:27 -0800

    commit

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,5 +1,7 @@
 
 
+SELECT * FROM dept_emp;
+
 --Return a list of employees eligable for retirement
 SELECT first_name, last_name
 FROM employees
@@ -67,7 +69,10 @@ retirement_info.last_name,
 FROM retirement_info
 LEFT JOIN dept_emp
 ON retirement_info.emp_no = dept_emp.emp_no;
+
 SELECT * FROM retirement_info;
+
+SELECT * FROM dept_emp;
 
 --name aliases
 --The to_date column returned NUL when I ran this.
@@ -111,13 +116,96 @@ LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 
---Using Count, Group By and Order by:
+DROP TABLE current_emp;
 
--- Employee count by department number
-SELECT COUNT(ce.emp_no), de.dept_no
+--Everything ran correctly up until this spot.  Data incorrect.
+--Inner Join for Departments and dept-manager Tables
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+--Left Join to capture retirement-info Table
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info	
+LEFT JOIN dept_emp	
+ON retirement_info.emp_no = dept_emp.emp_no;	
+	
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;	
+	
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info	
+LEFT JOIN dept_emp	
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+--Making an entirely new table from the beginning.  Checking to_date column
+SELECT * FROM salaries;
+
+--Looking for the most recent date on the list
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+--pulling employment dates from dept_emp
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--filtering for gender
+SELECT emp_no,
+	first_name,
+last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');	
+	
+--adding to_date and.
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+--INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)	
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)	
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')	
+     AND (de.to_date = '9999-01-01');	
+	
+--List Department etirees	
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+-- INTO dept_info	
 FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
-GROUP BY de.dept_no;
-
-
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);	
+	
